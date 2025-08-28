b8643d3f2179d77ae7365acd44039971b069a703
thanhdanhkode <bfffamilk@gmail.com>
2025-08-28 16:09:04 +0700

    feat: add cookie handler on request

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,42 @@ const { join } = require('node:path');
 const fs = require('node:fs');
 const CleanCSS = require('clean-css');
 const { minify } = require('terser');
+
+const allowAnonymous = (request, reply, done) => {
+  if (request.header.authorization) {
+    return done(Error('Not Anonymous'));
+  }
+  done();
+};
+
 const DefaultRoute = (fastify, option) => {
+  fastify.decorate('allowAnonymous', allowAnonymous);
+
+  fastify.addHook('onRequest', (request, reply, done) => {
+    const cookie = request.cookies;
+    const requiredCookies = ['starfrost.config', 'starfrost.lang', 'starfrost.session'];
+    let cookieSet = requiredCookies;
+    for (const key in cookie) {
+      if (requiredCookies.includes(key)) cookieSet = cookieSet.filter((item) => item !== key);
+    }
+    cookieSet.map((cookie) => {
+      reply.cookie(cookie, '');
+    });
+    done();
+  });
+
   fastify.get('/', async (request, reply) => {
-    return reply.status(200).send({ message: 'Starfrost is opened' });
+    const cookie = request.cookies;
+    const session = cookie['starfrost.session'];
+    if (!session) return reply.redirect('/login');
+    if (
+      session ===
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtlbHZpbiIsImlhdCI6MTc1NjI2MTI3N30.YLeGNhZr9QRY-Y4sNuFmGq28d6WbVu_JPTj6NUZYBpk'
+    )
+      return reply.redirect('/app');
+    else return reply.viewAsync('index.ejs', { title: 'Starfrost' });
+  });
+
   fastify.get('/:fileName(^\\w+).css', (request, reply) => {
     const { fileName } = request.params;
     const filePath = join(__dirname, '..', 'views', 'styles', `${fileName}.css`);
