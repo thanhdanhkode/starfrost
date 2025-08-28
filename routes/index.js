4589ffc70d26ad00f19e2ef7b8df76341949ae67
thanhdanhkode <bfffamilk@gmail.com>
2025-08-28 16:06:35 +0700

    feat(ui): add js and css minifier

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,25 @@
+const { join } = require('node:path');
+const fs = require('node:fs');
+const CleanCSS = require('clean-css');
+const { minify } = require('terser');
 const DefaultRoute = (fastify, option) => {
   fastify.get('/', async (request, reply) => {
     return reply.status(200).send({ message: 'Starfrost is opened' });
+  fastify.get('/:fileName(^\\w+).css', (request, reply) => {
+    const { fileName } = request.params;
+    const filePath = join(__dirname, '..', 'views', 'styles', `${fileName}.css`);
+    if (!fs.existsSync(filePath)) return reply.status(404).send({ error: 'File not found' });
+    const minifiedCSS = new CleanCSS({ inline: '!fonts.googleapis.com' }).minify([filePath]);
+    return reply.type('text/css').send(minifiedCSS.styles);
+  });
+
+  fastify.get('/:fileName(^\\w+).js', async (request, reply) => {
+    const { fileName } = request.params;
+    const filePath = join(__dirname, '..', 'views', 'js', `${fileName}.mjs`);
+    if (!fs.existsSync(filePath)) return reply.status(404).send({ error: 'File not found' });
+    const minifiedJS = await minify(fs.readFileSync(filePath, 'utf8'));
+    return reply.type('application/javascript').send(minifiedJS.code);
+  });
   });
 };
 
