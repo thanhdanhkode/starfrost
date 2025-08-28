ffb862c0b614c0fc0cd05a0c27dcc6038584441f
thanhdanhkode <bfffamilk@gmail.com>
2025-08-28 15:52:58 +0700

    chore: add fastify ecosystem and ejs

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 (async () => {
-	const fastify = require('fastify')({
-		logger: {
-			transport: {
-				target: 'pino-pretty',
-				options: {
-					colorize: true,
-					translateTime: 'SYS:HH:mm:ss',
-					ignore: 'pid,hostname,req,reqId,res,responseTime',
-				},
-			},
-		},
-	});
+  const fastify = require('fastify')({
+    logger: {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:HH:mm:ss',
+          ignore: 'pid,hostname,req,reqId,res,responseTime',
+        },
+      },
+    },
+  });
 
-	try {
-		await fastify.register(require('./server.js'));
-		await fastify.listen({ port: 3000 });
-	} catch (error) {
-		fastify.log.error(error);
-		process.exit(1);
-	}
+  try {
+    await fastify.register(require('./server.js'));
+    await fastify.listen({ port: 80 });
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
 })();
--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const server = async (fastify, options) => {
   await fastify.register(require('@fastify/websocket'));
 
 	await fastify.register(require('@fastify/auth'), {});
+  await fastify.register(require('@fastify/auth'), {});
 
   await fastify.register(require('@fastify/autoload'), {
     dir: join(__dirname, 'routes'),
     forceESM: true,
     routeParams: true,
+  await fastify.register(require('@fastify/view'), {
+    engine: { ejs: require('ejs') },
+    root: join(__dirname, 'views'),
   });
 
   await fastify.register(require('@fastify/env'), {
@@ -29,6 +33,7 @@ const server = async (fastify, options) => {
       console.error(err);
       return { error: err.message };
     },
+    addHook: false,
   });
 
   await fastify.register(require('@fastify/cookie'), {
@@ -37,11 +42,31 @@ const server = async (fastify, options) => {
     parseOptions: {},
   });
 
+  await fastify.register(require('@fastify/static'), {
+    root: join(__dirname, 'public'),
+    prefix: '/',
+  });
+
+  await fastify.register(require('@fastify/autoload'), {
+    dir: join(__dirname, 'routes'),
+    forceESM: true,
+    routeParams: true,
+  });
+
+  await fastify.register(require('@fastify/jwt'), {
+    secret: fastify.config.BEARER_TOKEN_SECRET,
+    cookie: {
+      cookieName: 'starfrost.session',
+    },
+  });
+
   fastify.ready(() => {
     console.log('[Starfrost] Routes\n', fastify.printRoutes());
+    // console.log('[Starfrost] Routes\n', fastify.printRoutes());
     // console.log('[Starfrost] Plugins \n', fastify.printPlugins());
     // console.log('[Starfrost] Environment Variables\n', fastify.getEnvs());
     console.log('[Starfrost] Ready!');
+    fastify.log.info('Ready!');
   });
 };
 
