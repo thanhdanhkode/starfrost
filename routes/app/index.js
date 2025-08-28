13dbd9bec2aa06e00bddd464ca3d875b7dd8f231
thanhdanhkode <bfffamilk@gmail.com>
2025-08-28 16:08:04 +0700

    feat(routes): add app routes

---
+++ routes/app/index.js
@@ -0,0 +1,203 @@
+const verifyAppJWT = (request, reply, done) => {
+  const cookie = request.cookies;
+
+  const token = cookie && cookie['starfrost.session'];
+
+  if (!token) return reply.redirect('/login');
+
+  request.jwtVerify({ onlyCookie: true });
+
+  done();
+};
+
+const AppRoute = (fastify, options) => {
+  fastify.decorate('verifyAppJWT', verifyAppJWT);
+
+  fastify.get('/', { preHandler: fastify.verifyAppJWT }, async (request, reply) => {
+    return reply.viewAsync('./pages/Home.ejs', { title: 'Starfrost' }, { layout: './partials/layouts/AppLayout.ejs' });
+  });
+
+  fastify.get('/instances', { preHandler: fastify.auth([fastify.verifyAppJWT]) }, async (request, reply) => {
+    return reply.viewAsync(
+      './pages/Instance.ejs',
+      {
+        title: 'Instances • Starfrost',
+        instanceData: [
+          {
+            id: 1,
+            name: 'Instance 1',
+            description: 'Instance Demo Testing for release',
+            cpu: 10,
+            cpuLimit: 100,
+            ram: 512,
+            ramLimit: 1024,
+            disk: 20,
+            diskLimit: 40,
+            status: 'running',
+          },
+          {
+            id: 2,
+            name: 'Instance 2',
+            description: 'Instance Demo Testing for release',
+            cpu: 10,
+            cpuLimit: 100,
+            ram: 512,
+            ramLimit: 1024,
+            disk: 20,
+            diskLimit: 40,
+            status: 'running',
+          },
+          {
+            id: 3,
+            name: 'Instance 3',
+            description: 'Instance Demo Testing for release',
+            cpu: 10,
+            cpuLimit: 100,
+            ram: 512,
+            ramLimit: 1024,
+            disk: 20,
+            diskLimit: 40,
+            status: 'running',
+          },
+          {
+            id: 4,
+            name: 'Instance 4',
+            description: 'Instance Demo Testing for release',
+            cpu: 10,
+            cpuLimit: 100,
+            ram: 512,
+            ramLimit: 1024,
+            disk: 20,
+            diskLimit: 40,
+            status: 'running',
+          },
+          {
+            id: 5,
+            name: 'Instance 5',
+            description: 'Instance Demo Testing for release',
+            cpu: 10,
+            cpuLimit: 100,
+            ram: 512,
+            ramLimit: 1024,
+            disk: 20,
+            diskLimit: 40,
+            status: 'running',
+          },
+        ],
+      },
+      { layout: './partials/layouts/AppLayout.ejs' },
+    );
+  });
+
+  fastify.get(
+    '/instances/:instanceId',
+    { preHandler: fastify.auth([fastify.verifyAppJWT]) },
+    async (request, reply) => {
+      return reply.viewAsync(
+        './pages/InstanceByIdOverview.ejs',
+        {
+          title: 'Instance Detail • Starfrost',
+          instanceData: { id: request.params.instanceId, name: 'Instance Name' },
+        },
+        { layout: './partials/layouts/InstanceLayout.ejs' },
+      );
+    },
+  );
+
+  fastify.get(
+    '/instances/:instanceId/overview',
+    { preHandler: fastify.auth([fastify.verifyAppJWT]) },
+    async (request, reply) => {
+      return reply.viewAsync(
+        './pages/InstanceByIdOverview.ejs',
+        {
+          title: 'Instance Detail • Starfrost',
+          instanceData: { id: request.params.instanceId, name: 'Instance Name' },
+        },
+        { layout: './partials/layouts/InstanceLayout.ejs' },
+      );
+    },
+  );
+  fastify.get(
+    '/instances/:instanceId/terminal',
+    { preHandler: fastify.auth([fastify.verifyAppJWT]) },
+    async (request, reply) => {
+      return reply.viewAsync(
+        './pages/InstanceByIdTerminal.ejs',
+        {
+          title: 'Instance Detail • Starfrost',
+          instanceData: { id: request.params.instanceId, name: 'Instance Name' },
+        },
+        { layout: './partials/layouts/InstanceLayout.ejs' },
+      );
+    },
+  );
+  fastify.get(
+    '/instances/:instanceId/files',
+    { preHandler: fastify.auth([fastify.verifyAppJWT]) },
+    async (request, reply) => {
+      return reply.viewAsync(
+        './pages/InstanceByIdFiles.ejs',
+        {
+          title: 'Instance Detail • Starfrost',
+          instanceData: { id: request.params.instanceId, name: 'Instance Name' },
+        },
+        { layout: './partials/layouts/InstanceLayout.ejs' },
+      );
+    },
+  );
+
+  fastify.get(
+    '/instances/:instanceId/settings',
+    { preHandler: fastify.auth([fastify.verifyAppJWT]) },
+    async (request, reply) => {
+      return reply.viewAsync(
+        './pages/InstanceByIdSettings.ejs',
+        {
+          title: 'Instance Detail • Starfrost',
+          instanceData: { id: request.params.instanceId, name: 'Instance Name' },
+        },
+        { layout: './partials/layouts/InstanceLayout.ejs' },
+      );
+    },
+  );
+
+  fastify.get('/users', { preHandler: fastify.auth([fastify.verifyAppJWT]) }, async (request, reply) => {
+    return reply.viewAsync(
+      './pages/User.ejs',
+      {
+        title: 'Users • Starfrost',
+        userData: [
+          {
+            id: 1,
+            name: 'User 1',
+            role: 'admin',
+          },
+          {
+            id: 2,
+            name: 'User 2',
+            role: 'normal',
+          },
+          {
+            id: 3,
+            name: 'User 3',
+            role: 'normal',
+          },
+          {
+            id: 4,
+            name: 'User 4',
+            role: 'normal',
+          },
+          {
+            id: 5,
+            name: 'User 5',
+            role: 'normal',
+          },
+        ],
+      },
+      { layout: './partials/layouts/AppLayout.ejs' },
+    );
+  });
+};
+
+module.exports = AppRoute;
--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,17 @@ const DefaultRoute = (fastify, option) => {
     const minifiedJS = await minify(fs.readFileSync(filePath, 'utf8'));
     return reply.type('application/javascript').send(minifiedJS.code);
   });
+
+  fastify.get('/login', {}, async (request, reply) => {
+    const cookie = request.cookies;
+    const session = cookie['starfrost.session'];
+    if (!session) return reply.viewAsync('./pages/Login.ejs', { title: 'Login' });
+    if (
+      session ===
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtlbHZpbiIsImlhdCI6MTc1NjI2MTI3N30.YLeGNhZr9QRY-Y4sNuFmGq28d6WbVu_JPTj6NUZYBpk'
+    )
+      return reply.redirect('/app');
+    return reply.viewAsync('./pages/Login.ejs', { title: 'Login' });
   });
 };
 
